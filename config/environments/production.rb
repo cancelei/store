0f0818ab67bc974a0537dba5d05d97a871c65834
cancelei <glauber.bannwart@gmail.com>
2024-12-25 21:31:18 -0300

    update production tutorial

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
   config.hosts.clear
   #
   # Skip DNS rebinding protection for the default health check endpoint.
-  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
